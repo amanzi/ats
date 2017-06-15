c59e799a0f2a6c5fd8f85cea2db7bd8c340f4071
Ethan Coon <coonet@ornl.gov>
2017-06-14 21:44:44 -0400

    fixes bugs blocking enthalpy of evaporation to be considered when removed from subsurface

--- src/pks/energy/two_phase/two_phase.hh
+++ src/pks/energy/two_phase/two_phase.hh
@@ -47,11 +47,6 @@ protected:
   //    faces.
   //  virtual void ApplyDirichletBCsToEnthalpy_(const Teuchos::Ptr<State>& S);
 
-  // -- No source terms needed.
-  virtual void AddSources_(const Teuchos::Ptr<State>& S,
-                           const Teuchos::Ptr<CompositeVector>& f) {}
-  virtual void AddSourcesToPrecon_(const Teuchos::Ptr<State>& S, double h) {}
-
 
  protected:
   // models for evaluating enthalpy
