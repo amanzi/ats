c82d37695e61a5712725dcef62be7e255e04829c
Chonggang Xu <xuchongang@gmail.com>
2020-04-07 17:48:05 -0600

    add the pre_canopyfluxes routine

--- src/pks/biogeochemistry/fates/fates_pk.cc
+++ src/pks/biogeochemistry/fates/fates_pk.cc
@@ -114,7 +114,7 @@ void FATES_PK::Setup(const Teuchos::Ptr<State>& S){
   S->RequireField(key_, name_)->SetMesh(mesh_surf_)
     ->SetComponent("cell", AmanziMesh::CELL, nlevsclass_);
 
-  patchno_ = plist_->get<int>("number of patches", 1);
+  patchno_ = plist_->get<int>("number of patches", 10);
   nlevdecomp_ = plist_->get<int>("number of decomposition levels", 1);
 
   // met_decomp_key_ = Keys::getKey(domain_surf_,"decomp_cpools_met");
@@ -508,7 +508,7 @@ bool FATES_PK::AdvanceStep(double t_old, double t_new, bool reinit){
     photo_input.solai[1] = 0.2*longwave_rad[0][0];    
     jday = 1.0 + (t_new - t_site_dym_)/dt_site_dym_;
 
-    prep_canopyfluxes(&clump_); //need to restore but has errors
+    prep_canopyfluxes(&clump_);
     wrap_sunfrac(&clump_, &radnum, photo_input.solad, photo_input.solai);
     wrap_canopy_radiation(&clump_, &jday, &radnum, photo_input.albgrd, photo_input.albgri);           
     wrap_photosynthesis( &clump_, &dt_photosynthesis_, &patm, &array_size, t_soil_.data(), &photo_input);  
