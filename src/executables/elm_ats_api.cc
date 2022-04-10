c634f5de38cfc7773234c0e4681785e3d231df6f
Joe Beisman <jjbeisman@gmail.com>
2022-04-10 04:47:08 -0400

    basic c api to expose driver functionality

---
+++ src/executables/elm_ats_api.cc
@@ -0,0 +1,35 @@
+
+#include "elm_ats_driver.hh"
+#include "elm_ats_api.h"
+
+#ifdef __cplusplus
+extern "C"{
+#endif
+
+// allocate, call constructor and cast ptr to opaque ELM_ATS_DRIVER
+ELM_ATS_DRIVER* ats_create() {
+  return reinterpret_cast<ELM_ATS_DRIVER*>(new ATS::ELM_ATSDriver());
+}
+// reinterpret as elm_ats_driver and delete (calls destructor)
+void ats_delete(ELM_ATS_DRIVER *ats) {
+  delete reinterpret_cast<ATS::ELM_ATSDriver*>(ats);
+}
+// call driver setup()
+int ats_setup(ELM_ATS_DRIVER *ats, char *input_filename) {
+  return reinterpret_cast<ATS::ELM_ATSDriver*>(ats)->setup(input_filename);
+}
+// call driver initialize()
+void ats_initialize(ELM_ATS_DRIVER *ats){
+  return reinterpret_cast<ATS::ELM_ATSDriver*>(ats)->initialize();
+}
+// call driver advance(dt)
+void ats_advance(ELM_ATS_DRIVER *ats, double *dt) {
+  return reinterpret_cast<ATS::ELM_ATSDriver*>(ats)->advance(dt);
+}
+// call driver advance_test()
+void ats_advance_test(ELM_ATS_DRIVER *ats) {
+  return reinterpret_cast<ATS::ELM_ATSDriver*>(ats)->advance_test();
+}
+#ifdef __cplusplus
+}
+#endif
---
+++ src/executables/elm_ats_api.h
@@ -0,0 +1,25 @@
+
+#ifdef __cplusplus
+extern "C" {
+    class ELM_ATSDriver;
+    typedef ELM_ATSDriver ELM_ATS_DRIVER;
+#else
+    typedef struct ELM_ATS_DRIVER ELM_ATS_DRIVER;
+#endif
+
+// allocate, call constructor and cast ptr to opaque ELM_ATS_DRIVER
+ELM_ATS_DRIVER* ats_create();
+// reinterpret as elm_ats_driver and delete (calls destructor)
+void ats_delete(ELM_ATS_DRIVER *ats);
+// call driver setup()
+int ats_setup(ELM_ATS_DRIVER *ats, char *input_filename);
+// call driver initialize()
+void ats_initialize(ELM_ATS_DRIVER *ats);
+// call driver advance(dt)
+void ats_advance(ELM_ATS_DRIVER *ats, double *dt);
+// call driver advance_test()
+void ats_advance_test(ELM_ATS_DRIVER *ats);
+
+#ifdef __cplusplus
+}
+#endif
