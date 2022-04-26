e8f5298ed4dd7781771bc4f02270b51e6d7641ea
Gao B <3hg@or-slurm-login06.ornl.gov>
2022-04-26 13:58:54 -0400

    add depression storage evaluator

---
+++ src/pks/flow/constitutive_relations/elevation/subgrid_mobile_depth_evaluator_reg.hh
@@ -0,0 +1,12 @@
+#include "subgrid_mobile_depth_evaluator.hh"
+
+namespace Amanzi {
+namespace Flow {
+namespace FlowRelations {
+
+// registry of method
+Utils::RegisteredFactory<FieldEvaluator,SubgridMobileDepthEvaluator> SubgridMobileDepthEvaluator::factory_("subgrid mobile depth");
+
+}
+}
+}
