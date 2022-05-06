4c6ad470b1ee80a8bfa89841a882a01bc9e51d5a
Yuan, Fengming <fmyuan@gmail.com>
2022-05-06 17:59:12 -0400

    fix keys' names for overland flow/flow pks' source/sink term.

--- src/pks/flow/richards_pk.cc
+++ src/pks/flow/richards_pk.cc
@@ -322,7 +322,7 @@ void Richards::SetupRichardsFlow_()
   downreg_source_term_ = plist_->get<bool>("down-regulating source term", false);
   if (is_source_term_) {
     if (source_key_.empty())
-      source_key_ = Keys::readKey(*plist_, domain_, "source", "water_source");
+      source_key_ = Keys::readKey(*plist_, domain_, "source", "source_sink");
     source_term_is_differentiable_ =
         plist_->get<bool>("source term is differentiable", true);
     explicit_source_ = plist_->get<bool>("explicit source term", false);
