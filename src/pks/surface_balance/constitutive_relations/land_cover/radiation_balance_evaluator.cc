148ba3043d883f06e4d4ff5803d7cdeae43af36c
Ethan Coon <coonet@ornl.gov>
2022-08-05 10:37:12 -0400

    typo allows NaNs in run

--- src/pks/surface_balance/constitutive_relations/land_cover/radiation_balance_evaluator.cc
+++ src/pks/surface_balance/constitutive_relations/land_cover/radiation_balance_evaluator.cc
@@ -67,7 +67,7 @@ RadiationBalanceEvaluator::EnsureCompatibility(const Teuchos::Ptr<State>& S)
 {
   if (!compatible_) {
     land_cover_ = getLandCover(S->ICList().sublist("land cover types"),
-            {"beers_law_lw", "beers_law_sw", "emissivity_canopy", "albedo_canopy"});
+            {"beers_k_lw", "beers_k_sw", "emissivity_canopy", "albedo_canopy"});
 
     for (const auto& my_key : my_keys_) {
       // require all domains are the same
