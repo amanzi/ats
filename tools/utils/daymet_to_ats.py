d43ca657fd430e90c35a4a1fc8e8cbd7a5078ed8
Bo Gao <83304414+gaobhub@users.noreply.github.com>
2022-09-02 17:02:06 -0400

    revise saturated vapor pressure, use vapor pressure air in input, clean code

--- tools/utils/daymet_to_ats.py
+++ tools/utils/daymet_to_ats.py
@@ -82,9 +82,6 @@ def daymet_to_ats(dat):
     mean_air_temp_c = (dat['tmin_deg_c'] + dat['tmax_deg_c'])/2.0
     precip_ms = dat['prcp_mmday'] / 1.e3 / 86400.
     
-    # Sat vap. press o/water Dingman D-7 (Bolton, 1980)
-    # sat_vp_Pa = 611.2 * np.exp(17.67 * mean_air_temp_c / (mean_air_temp_c + 243.5))
-
     dout['time [s]'] = np.arange(0, len(dat), 1)*86400.
     dout['air temperature [K]'] = 273.15 + mean_air_temp_c
     dout['incoming shortwave radiation [W m^-2]'] = dat['dayl_s']/86400*dat['srad_Wm2']
