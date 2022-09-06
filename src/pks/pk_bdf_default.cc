9acd6ec414ac511294a2dc88ebdd467806278d85
Ethan Coon <coonet@ornl.gov>
2022-09-06 11:51:35 -0400

    fixes a few bugs in unique names for storing dt, solution history in state

--- src/pks/pk_bdf_default.cc
+++ src/pks/pk_bdf_default.cc
@@ -49,19 +49,19 @@ void PK_BDF_Default::Setup()
 void PK_BDF_Default::Initialize()
 {
   if (!strongly_coupled_) {
-    // initialize the timestep
-    double dt = plist_->get<double>("initial time step [s]", 1.);
-    S_->Assign("dt", Tag(name_), name_, dt);
-    S_->GetRecordW("dt", Tag(name_), name_).set_initialized();
-
     // set up the timestepping algorithm
     // -- construct the time integrator
     //   Note, this is done here and not in setup because solution is not ready in setup
     Teuchos::ParameterList& bdf_plist = plist_->sublist("time integrator");
-    if (!bdf_plist.isSublist("verbose object"))
-      bdf_plist.set("verbose object", plist_->sublist("verbose object"));
+    bdf_plist.sublist("verbose object").setParametersNotAlreadySet(plist_->sublist("verbose object"));
+    bdf_plist.sublist("verbose object").set("name", name());
+
     time_stepper_ = Teuchos::rcp(new BDF1_TI<TreeVector,TreeVectorSpace>(*this,
             bdf_plist, solution_, S_));
+
+    double dt_init = time_stepper_->initial_timestep();
+    S_->Assign("dt", Tag(name_), name_, dt_init);
+    S_->GetRecordW("dt", Tag(name_), name_).set_initialized();
 
     // -- initialize continuation parameter if needed.
     if (S_->HasRecord("continuation_parameter", Tag(name_))) {
