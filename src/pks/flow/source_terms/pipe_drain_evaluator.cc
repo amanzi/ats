473495d72dd17017387ae0de3fe5d7377f45047c
Giacomo Capodaglio <gcapodaglio@pn2003029.lanl.gov>
2022-11-21 19:57:31 -0700

    Adding evaluator for pipe drainage network
    Work in progress

---
+++ src/pks/flow/source_terms/pipe_drain_evaluator.cc
@@ -0,0 +1,131 @@
+/* -*-  mode: c++; indent-tabs-mode: nil -*- */
+
+/*
+  Evaluator for water drainage through a pipe network.
+  This depends on:
+  1) flow depth above surface elevation
+  2) hydraulic head in the pipe network
+
+  Authors: Giacomo Capodaglio (gcapodaglio@lanl.gov)
+*/
+
+#include "pipe_drain_evaluator.hh"
+
+namespace Amanzi {
+namespace Flow {
+
+
+PipeDrainEvaluator::PipeDrainEvaluator(Teuchos::ParameterList& plist) :
+    EvaluatorSecondaryMonotypeCV(plist)
+{
+  M_ = plist_.get<double>("molar mass", 0.0180153);
+  bar_ = plist_.get<bool>("allow negative water content", false);
+  rollover_ = plist_.get<double>("water content rollover", 0.);
+
+  manhole_radius_ = plist_.get<double>("manhole radius", 0.24);
+  energy_losses_coeff_ = plist.get<double>("energy losses coeff", 0.1);
+  H_max_ = plist.get<double>("pipe max height", 0.1);
+  gravity_ = plist.get<double>("gravity", 9.81);//TODO this needs to be converted to an array
+  H_ = plist.get<double>("bed flume height", 0.478);
+
+  Key domain_name = Keys::getDomain(my_keys_.front().first);
+  Tag tag = my_keys_.front().second;
+
+  // TODO
+  // my dependencies
+  //pres_key_ = Keys::readKey(plist_, domain_name, "pressure", "pressure");
+  //dependencies_.insert(KeyTag{pres_key_, tag});
+  //cv_key_ = Keys::readKey(plist_, domain_name, "cell volume", "cell_volume");
+  //dependencies_.insert(KeyTag{cv_key_, tag});
+}
+
+
+Teuchos::RCP<Evaluator>
+PipeDrainEvaluator::Clone() const {
+  return Teuchos::rcp(new PipeDrainEvaluator(*this));
+}
+
+
+void PipeDrainEvaluator::Evaluate_(const State& S,
+        const std::vector<CompositeVector*>& result)
+{
+  Tag tag = my_keys_.front().second;
+  Epetra_MultiVector& res = *result[0]->ViewComponent("cell",false);
+  const Epetra_MultiVector& pres = *S.GetPtr<CompositeVector>(pres_key_, tag)
+      ->ViewComponent("cell",false);
+
+  const Epetra_MultiVector& cv = *S.GetPtr<CompositeVector>(cv_key_, tag)
+      ->ViewComponent("cell",false);
+
+  const double& p_atm = S.Get<double>("atmospheric_pressure", Tags::DEFAULT);
+  const auto& gravity = S.Get<AmanziGeometry::Point>("gravity", Tags::DEFAULT);
+  double gz = -gravity[2];  // check this
+
+  int ncells = res.MyLength();
+  if (bar_) {
+    for (int c=0; c!=ncells; ++c) {
+      res[0][c] = cv[0][c] * (pres[0][c] - p_atm) / (gz * M_);
+    }
+  } else if (rollover_ > 0.) {
+    for (int c=0; c!=ncells; ++c) {
+      double dp = pres[0][c] - p_atm;
+      double dp_eff = dp < 0. ? 0. :
+          dp < rollover_ ?
+            dp*dp/(2*rollover_) :
+            dp - rollover_/2.;
+      res[0][c] = cv[0][c] * dp_eff / (gz * M_);
+    }
+  } else {
+    for (int c=0; c!=ncells; ++c) {
+      res[0][c] = pres[0][c] < p_atm ? 0. :
+          cv[0][c] * (pres[0][c] - p_atm) / (gz * M_);
+    }
+  }
+}
+
+
+void OverlandPressureWaterContentEvaluator::EvaluatePartialDerivative_(const State& S,
+        const Key& wrt_key, const Tag& wrt_tag,
+        const std::vector<CompositeVector*>& result)
+{
+  Tag tag = my_keys_.front().second;
+  AMANZI_ASSERT(wrt_key == pres_key_);
+
+  Epetra_MultiVector& res = *result[0]->ViewComponent("cell",false);
+  const Epetra_MultiVector& pres = *S.GetPtr<CompositeVector>(pres_key_, tag)
+      ->ViewComponent("cell",false);
+
+  const Epetra_MultiVector& cv = *S.GetPtr<CompositeVector>(cv_key_, tag)
+      ->ViewComponent("cell",false);
+
+  const double& p_atm = S.Get<double>("atmospheric_pressure", Tags::DEFAULT);
+  const auto& gravity = S.Get<AmanziGeometry::Point>("gravity", Tags::DEFAULT);
+  double gz = -gravity[2];  // check this
+
+  if (wrt_key == pres_key_) {
+    int ncells = res.MyLength();
+    if (bar_) {
+      for (int c=0; c!=ncells; ++c) {
+        res[0][c] = cv[0][c] / (gz * M_);
+      }
+    } else if (rollover_ > 0.) {
+      for (int c=0; c!=ncells; ++c) {
+        double dp = pres[0][c] - p_atm;
+        double ddp_eff = dp < 0. ? 0. :
+          dp < rollover_ ? dp/rollover_ : 1.;
+        res[0][c] = cv[0][c] * ddp_eff / (gz * M_);
+      }
+    } else {
+      for (int c=0; c!=ncells; ++c) {
+        res[0][c] = pres[0][c] < p_atm ? 0. :
+          cv[0][c] / (gz * M_);
+      }
+    }
+  } else {
+    res.PutScalar(0.);
+  }
+}
+
+
+} //namespace
+} //namespace
---
+++ src/pks/flow/source_terms/pipe_drain_evaluator.hh
@@ -0,0 +1,59 @@
+/* -*-  mode: c++; indent-tabs-mode: nil -*- */
+
+/*
+  Evaluator for water drainage through a pipe network.
+  This depends on:
+  1) flow depth above surface elevation
+  2) hydraulic head in the pipe network
+
+  Authors: Giacomo Capodaglio (gcapodaglio@lanl.gov)
+*/
+
+#ifndef AMANZI_FLOW_RELATIONS_PIPE_DRAIN_EVALUATOR_
+#define AMANZI_FLOW_RELATIONS_PIPE_DRAIN_EVALUATOR_
+
+#include "EvaluatorSecondaryMonotype.hh"
+#include "Factory.hh"
+
+namespace Amanzi {
+namespace Flow {
+
+class PipeDrainEvaluator : public EvaluatorSecondaryMonotypeCV {
+
+ public:
+  // constructor format for all derived classes
+  explicit
+  PipeDrainEvaluator(Teuchos::ParameterList& plist);
+  PipeDrainEvaluator(const PipeDrainEvaluator& other) = default;
+
+  virtual Teuchos::RCP<Evaluator> Clone() const override;
+
+ protected:
+  void InitializeFromPlist_();
+
+  // Required methods from EvaluatorSecondaryMonotypeCV
+  virtual void Evaluate_(const State& S,
+          const std::vector<CompositeVector*>& result) override;
+  virtual void EvaluatePartialDerivative_(const State& S,
+          const Key& wrt_key, const Tag& wrt_tag,
+          const std::vector<CompositeVector*>& result) override;
+
+ protected:
+ //What are the names for the flow depth above surface and hydraulic head in pipe network?
+ //Key pres_key_, cv_key_;
+
+  double manhole_radius_;
+  double energy_losses_coeff_; //at manhole
+  double H_max_; //max height of pipe (considering a rectangular cross section)
+  double gravity_; //how to say this is an array? std vectors?
+  double H_; //surface elevation relative to the pipe flow hydraulic head
+
+ private:
+  static Utils::RegisteredFactory<Evaluator,PipeDrainEvaluator> reg_;
+
+};
+
+} //namespace
+} //namespace
+
+#endif
