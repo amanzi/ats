69bca38404c7d17cd166549b7f86087c94eb6da3
Joe Beisman <jjbeisman@gmail.com>
2023-03-15 10:10:47 -0400

    Fix from merge - replace registration includes with ats_registrations file

--- src/executables/elm_ats_api/elm_ats_driver.cc
+++ src/executables/elm_ats_api/elm_ats_driver.cc
@@ -13,23 +13,7 @@
 #include "VerboseObject.hh"
 
 // registration files
-#include "state_evaluators_registration.hh"
-#include "ats_relations_registration.hh"
-#include "ats_transport_registration.hh"
-#include "ats_energy_pks_registration.hh"
-#include "ats_energy_relations_registration.hh"
-#include "ats_flow_pks_registration.hh"
-#include "ats_flow_relations_registration.hh"
-#include "ats_deformation_registration.hh"
-#include "ats_bgc_registration.hh"
-#include "ats_surface_balance_registration.hh"
-#include "ats_mpc_registration.hh"
-//#include "ats_sediment_transport_registration.hh"
-#include "mdm_transport_registration.hh"
-#include "multiscale_transport_registration.hh"
-#ifdef ALQUIMIA_ENABLED
-#include "pks_chemistry_registration.hh"
-#endif
+#include "ats_registration_files.hh"
 
 // include fenv if it exists
 #include "boost/version.hpp"
