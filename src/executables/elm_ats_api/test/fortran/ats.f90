5ea12f0c979131029965c235544fc8fb24e36aa4
Joe Beisman <jjbeisman@gmail.com>
2023-03-16 02:43:34 -0400

    Got Fortran driver test working

--- src/executables/elm_ats_api/test/fortran/ats.f90
+++ src/executables/elm_ats_api/test/fortran/ats.f90
@@ -1,11 +1,15 @@
 
-!! test interface
+!! Defines a Fortran interface to ATS library
+!! Provides Fortran method declarations and bindings to ATS C API functions
+!! The implementation of the methods declared here can be found in ats_mod.f90
 interface
 
-    function ats_create_c() bind(c, name="ats_create")
+    function ats_create_c(comm, infile) bind(c, name="ats_create")
         use iso_c_binding
         implicit none
         type(c_ptr) :: ats_create_c
+        integer, intent(in) :: comm
+        character(len=1, kind=C_CHAR), intent(in) :: infile(*)
     end function
     
     subroutine ats_delete_c(ats) bind(c, name="ats_delete")
@@ -14,12 +18,10 @@ interface
         type(c_ptr) :: ats
     end subroutine
     
-    subroutine ats_setup_c(ats, comm, infile) bind(c, name="ats_setup")
+    subroutine ats_setup_c(ats) bind(c, name="ats_setup")
         use iso_c_binding
         implicit none
         type(c_ptr), value :: ats
-        integer, intent(in) :: comm
-        character(len=1, kind=C_CHAR), intent(in) :: infile(*)
     end subroutine
     
     subroutine ats_initialize_c(ats) bind(c, name="ats_initialize")
@@ -41,7 +43,8 @@ interface
         real(c_double), intent(in) :: dt
     end subroutine
 
-    subroutine ats_set_sources_c(ats, soil_infil, soil_evap, root_trans, ncols, ncells) bind(c, name="ats_set_sources")
+    subroutine ats_set_sources_c(ats, soil_infil, soil_evap, root_trans, ncols, ncells) &
+        bind(c, name="ats_set_sources")
         use iso_c_binding
         implicit none
         type(c_ptr), value :: ats
@@ -52,7 +55,8 @@ interface
         integer(c_int), intent(in) :: ncells
     end subroutine
 
-    subroutine ats_get_waterstate_c(ats, surf_pres, soil_pres, satur, ncols, ncells) bind(c, name="ats_get_waterstate")
+    subroutine ats_get_waterstate_c(ats, surf_pres, soil_pres, satur, ncols, ncells) &
+        bind(c, name="ats_get_waterstate")
         use iso_c_binding
         implicit none
         type(c_ptr), value :: ats
@@ -63,8 +67,8 @@ interface
         integer(c_int), intent(in) :: ncells
     end subroutine
 
-    subroutine ats_get_mesh_info_c(ats, ncols_local, ncols_global, ncells_per_col, dz, depth, elev, surf_area_m2, lat, lon) &
-    bind(c, name="ats_get_mesh_info")
+    subroutine ats_get_mesh_info_c(ats, ncols_local, ncols_global, ncells_per_col, dz, &
+        depth, elev, surf_area_m2, lat, lon) bind(c, name="ats_get_mesh_info")
         use iso_c_binding
         implicit none
         type(c_ptr), value :: ats
--- src/executables/elm_ats_api/test/fortran/ats_mod.f90
+++ src/executables/elm_ats_api/test/fortran/ats_mod.f90
@@ -1,10 +1,10 @@
 
+!! Defines a Fortran type that can store a pointer to ATS
+!! Provides implementations of methods declared in ats.f90
 module libats
     use iso_c_binding
-
     private
     public :: ats
-
     include "ats.f90"
 
     type ats
@@ -26,43 +26,52 @@ module libats
         procedure ats_create
     end interface
 
+!! Definitions of Fortran interface methods
+!! These methods act like wrappers around their C bindings.
 contains
     
-    function ats_create()
+    !! Allocate and construct a new ATS object
+    !! Returns a pointer to new ATS object
+    function ats_create(comm, infile)
         implicit none
         type(ats) :: ats_create
-        ats_create%ptr = ats_create_c()
-    end function
-
-    subroutine ats_delete(this)
-        implicit none
-        type(ats) :: this
-        call ats_delete_c(this%ptr)
-    end subroutine
-
-    subroutine ats_setup(this, comm, infile)
-        implicit none
-        class(ats) :: this
         integer, intent(in) :: comm
         character(len=*), intent(in) :: infile
         character(len=1, kind=C_CHAR) :: c_str_infile(len_trim(infile) + 1)
         integer :: n_char, i
 
+        !! basic parsing
         n_char = len_trim(infile)
         do i = 1, n_char
             c_str_infile(i) = infile(i:i)
         end do
         c_str_infile(n_char + 1) = C_NULL_CHAR
 
-       call ats_setup_c(this%ptr, comm, c_str_infile)
+        ats_create%ptr = ats_create_c(comm, c_str_infile)
+    end function
+
+    !! Destroy ATS object
+    subroutine ats_delete(this)
+        implicit none
+        type(ats) :: this
+        call ats_delete_c(this%ptr)
     end subroutine
 
+    !! Setup ATS for LSM simulation
+    subroutine ats_setup(this)
+        implicit none
+        class(ats) :: this
+       call ats_setup_c(this%ptr)
+    end subroutine
+
+    !! Initialize ATS for this simulation
     subroutine ats_initialize(this)
         implicit none
         class(ats) :: this
         call ats_initialize_c(this%ptr)
     end subroutine
 
+    !! Advance ATS by dt
     subroutine ats_advance(this, dt)
         implicit none
         class(ats) :: this
@@ -70,12 +79,15 @@ contains
         call ats_advance_c(this%ptr, dt)
     end subroutine
 
+    !! Temporary for testing - implements time loop and calls ats_advance()
     subroutine ats_advance_test(this)
         implicit none
         class(ats) :: this
         call ats_advance_test_c(this%ptr)
     end subroutine
 
+    !! WIP
+    !! Sets ATS coupling variables infiltration, evaporation, and transpiration
     subroutine ats_set_sources(this, soil_infil, soil_evap, root_trans, ncols, ncells)
         implicit none
         class(ats) :: this
@@ -87,6 +99,7 @@ contains
         call ats_set_sources_c(this%ptr, soil_infil, soil_evap, root_trans, ncols, ncells)
     end subroutine
 
+    !! WIP
     subroutine ats_get_waterstate(this, surf_pres, soil_pres, satur, ncols, ncells)
         implicit none
         class(ats) :: this
@@ -98,6 +111,7 @@ contains
         call ats_get_waterstate_c(this%ptr, surf_pres, soil_pres, satur, ncols, ncells)
     end subroutine
 
+    !! WIP
     subroutine ats_get_mesh_info(this, ncols_local, ncols_global, ncells_per_col, dz, depth, elev, surf_area_m2, lat, lon)
         implicit none
         class(ats) :: this
