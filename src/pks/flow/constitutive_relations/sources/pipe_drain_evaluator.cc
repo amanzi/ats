e248f68bf4334623d80b0fdc2e3c9be2468682e5
Giacomo Capodaglio <gcapodaglio@pn2003029.lanl.gov>
2023-05-05 15:23:51 -0600

    Fixed gravity in pipe drain evaluator

--- src/pks/flow/constitutive_relations/sources/pipe_drain_evaluator.cc
+++ src/pks/flow/constitutive_relations/sources/pipe_drain_evaluator.cc
@@ -29,6 +29,7 @@ PipeDrainEvaluator::PipeDrainEvaluator(Teuchos::ParameterList& plist) :
 
   // my dependencies
   head_key_ = Keys::readKey(plist_, domain_name, "ponded depth", "ponded_depth");
+
   dependencies_.insert(KeyTag{head_key_, tag});
 
   mark_key_ = Keys::readKey(plist_, domain_name, "manhole locations", "manhole_locations");
@@ -56,14 +57,14 @@ void PipeDrainEvaluator::Evaluate_(const State& S,
       ->ViewComponent("cell",false);
 
   const auto& gravity = S.Get<AmanziGeometry::Point>("gravity", Tags::DEFAULT);
-  double gz = -gravity[2];  // check this
+  double gz = -gravity[1]; 
   double pi = 3.14159265359;
   double manhole_area_ = pi * manhole_radius_ * manhole_radius_;
 
   int ncells = res.MyLength();
   for (int c=0; c!=ncells; ++c) {
     //if (hp < H_) {
-     res[0][c] = - mark[0][c] *  4.0 / 3.0 * energy_losses_coeff_ * pi * manhole_radius_ * sqrt(2.0 * gz) * pow(head[0][c],3.0/2.0) / manhole_area_; 
+     res[0][c] = - mark[0][c] *  4.0 / 3.0 * energy_losses_coeff_ * pi * manhole_radius_ * sqrt(2.0 * gz) * pow(head[0][c],3.0/2.0) / manhole_area_;
           // } else if (H_ < hp[c] && hp < (H_ + head[0][c]) ){
           //res[0][c] = - energy_losses_coeff_ * manhole_area_ * sqrt(2.0 * gz) * sqrt(head[0][c] + H_ - hp[c]);   
           //} else if (hp > (H_ + head[0][c])) {
