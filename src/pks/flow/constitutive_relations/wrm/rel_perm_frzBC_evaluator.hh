82a80f8cf8821d675d0398b755a529789170aba6
Ethan Coon <coonet@ornl.gov>
2023-06-30 16:11:11 -0400

    correct author

--- src/pks/flow/constitutive_relations/wrm/rel_perm_frzBC_evaluator.hh
+++ src/pks/flow/constitutive_relations/wrm/rel_perm_frzBC_evaluator.hh
@@ -4,7 +4,7 @@
   The terms of use and "as is" disclaimer for this license are
   provided in the top-level COPYRIGHT file.
 
-  Authors: Ethan Coon (ecoon@lanl.gov)
+  Authors: Bo Gao (gaob@ornl.gov)
 */
 
 //! Evaluates relative permeability using water retention models.
