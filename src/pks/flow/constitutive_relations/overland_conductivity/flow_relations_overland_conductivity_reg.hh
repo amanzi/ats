198e85ff4620ccf78de3dd040de2a3a647747783
Ethan Coon <coonet@ornl.gov>
2023-08-11 09:25:24 -0400

    progress toward coupled water

---
+++ src/pks/flow/constitutive_relations/overland_conductivity/flow_relations_overland_conductivity_reg.hh
@@ -0,0 +1,12 @@
+#include "Factory.hh"
+
+#include "registration_macro.hh"
+
+#include "overland_conductivity_evaluator.hh"
+
+namespace Amanzi {
+
+REGISTER(Flow::Relations::OverlandConductivityEvaluator);
+
+
+} // namespace Amanzi
