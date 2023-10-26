4f7186bef249f6eb51e655f799466d6e4e7a6df6
saubhagya-gatech <saubhagya.rathore@outlook.com>
2023-10-26 13:35:42 -0400

    WIP gate evaluator files
    wip
    
    fixes to the evaluator
    
    first-cut running evaluator
    
    checking water demand and supply
    
    pending pump commits
    
    added pump trigger and related  parameters
    
    detention pond now referred to as storage area
    
    cleanup

---
+++ src/pks/flow/constitutive_relations/sources/surface_gate_structure_evaluator_reg.hh
@@ -0,0 +1,21 @@
+/*
+  Copyright 2010-202x held jointly by participating institutions.
+  ATS is released under the three-clause BSD License.
+  The terms of use and "as is" disclaimer for this license are
+  provided in the top-level COPYRIGHT file.
+
+  Authors:
+*/
+
+#include "surface_gate_structure_evaluator.hh"
+
+namespace Amanzi {
+namespace Flow {
+namespace Relations {
+
+Utils::RegisteredFactory<Evaluator, SurfGateEvaluator>
+  SurfGateEvaluator::reg_("gate structure");
+
+} // namespace Relations
+} // namespace Flow
+} // namespace Amanzi
