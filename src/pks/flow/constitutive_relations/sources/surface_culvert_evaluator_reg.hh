0008dc1e7585e86c03391db2d775de4cd1f902c1
saubhagya-gatech <saubhagya.rathore@outlook.com>
2023-10-26 13:35:42 -0400

    Adds evaluators for gates, pumps, culverts, and impervious interception
    - gate: new evaluator, new parameters based on gate flow model/gate
      curve
    - pumps: new evaluator with parameters for pump efficiency curves
    - culverts: new evaluator to short-circuit overland flow via an
      implicit approach
    - impevious interception: non-local source-sink pairs to move rainfall
      into ditches directly without touching surface system
    
    - documentation is wip

---
+++ src/pks/flow/constitutive_relations/sources/surface_culvert_evaluator_reg.hh
@@ -0,0 +1,21 @@
+/*
+  Copyright 2010-202x held jointly by participating institutions.
+  ATS is released under the three-clause BSD License.
+  The terms of use and "as is" disclaimer for this license are
+  provided in the top-level COPYRIGHT file.
+
+  Authors:
+*/
+
+#include "surface_culvert_evaluator.hh"
+
+namespace Amanzi {
+namespace Flow {
+namespace Relations {
+
+Utils::RegisteredFactory<Evaluator, SurfCulvertEvaluator>
+  SurfCulvertEvaluator::reg_("culvert");
+
+} // namespace Relations
+} // namespace Flow
+} // namespace Amanzi
