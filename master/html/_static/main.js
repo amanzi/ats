9b74626fc3ab8ea50832ad15d32a58108b5b72c0
Ethan Coon <coonet@ornl.gov>
2024-06-07 14:31:06 -0600

    rebuild documentation with version picker

--- master/html/_static/main.js
+++
@@ -1,8 +0,0 @@
-$(document).ready( function () {
-    $('table.datatable').DataTable({
-        paging: false,
-        scrollCollapse: true,
-        scrollY: '800px',
-        order: [[4,'asc'],[0,'asc']],
-    }).columns.align();
-});
