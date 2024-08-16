08573034094d9f6fd4739bf49c35f0f38ad6d182
Ethan Coon <coonet@ornl.gov>
2024-08-16 14:24:13 -0600

    clean name preserving domain name (#268)

--- src/pks/pk_bdf_default.cc
+++ src/pks/pk_bdf_default.cc
@@ -39,11 +39,11 @@ PK_BDF_Default::Setup()
     // check if continuation method and require continuation parameter
     // -- ETC Note this needs fixed if more than one continuation method used
     if (bdf_plist.isSublist("continuation parameters")) {
-      S_->Require<double>(Keys::cleanName(name_) + "_continuation_parameter", Tags::DEFAULT, name_);
+      S_->Require<double>(Keys::cleanName(name_, true) + "_continuation_parameter", Tags::DEFAULT, name_);
     }
 
     // require data for checkpointing timestep size
-    S_->Require<double>(Keys::cleanName(name_) + "_dt_internal", Tags::DEFAULT, name_);
+    S_->Require<double>(Keys::cleanName(name_, true) + "_dt_internal", Tags::DEFAULT, name_);
   }
 };
 
@@ -67,14 +67,14 @@ PK_BDF_Default::Initialize()
       Teuchos::rcp(new BDF1_TI<TreeVector, TreeVectorSpace>(*this, bdf_plist, solution_, S_));
 
     double dt_init = time_stepper_->initial_timestep();
-    S_->Assign(Keys::cleanName(name_) + "_dt_internal", Tags::DEFAULT, name_, dt_init);
-    S_->GetRecordW(Keys::cleanName(name_) + "_dt_internal", Tags::DEFAULT, name_).set_initialized();
+    S_->Assign(Keys::cleanName(name_, true) + "_dt_internal", Tags::DEFAULT, name_, dt_init);
+    S_->GetRecordW(Keys::cleanName(name_, true) + "_dt_internal", Tags::DEFAULT, name_).set_initialized();
 
     // -- initialize continuation parameter if needed.
-    if (S_->HasRecord(Keys::cleanName(name_) + "_continuation_parameter", Tags::DEFAULT)) {
+    if (S_->HasRecord(Keys::cleanName(name_, true) + "_continuation_parameter", Tags::DEFAULT)) {
       S_->Assign(
-        Keys::cleanName(name_) + "_continuation_parameter", Tags::DEFAULT, name_, (double)1.);
-      S_->GetRecordW(Keys::cleanName(name_) + "_continuation_parameter", Tags::DEFAULT, name_)
+        Keys::cleanName(name_, true) + "_continuation_parameter", Tags::DEFAULT, name_, (double)1.);
+      S_->GetRecordW(Keys::cleanName(name_, true) + "_continuation_parameter", Tags::DEFAULT, name_)
         .set_initialized();
     }
 
@@ -95,7 +95,7 @@ double
 PK_BDF_Default::get_dt()
 {
   if (!strongly_coupled_)
-    return S_->Get<double>(Keys::cleanName(name_) + "_dt_internal", Tags::DEFAULT);
+    return S_->Get<double>(Keys::cleanName(name_, true) + "_dt_internal", Tags::DEFAULT);
   else
     return -1.;
 }
@@ -104,7 +104,7 @@ void
 PK_BDF_Default::set_dt(double dt)
 {
   if (!strongly_coupled_)
-    S_->Assign(Keys::cleanName(name_) + "_dt_internal", Tags::DEFAULT, name_, dt);
+    S_->Assign(Keys::cleanName(name_, true) + "_dt_internal", Tags::DEFAULT, name_, dt);
 }
 
 // -- Commit any secondary (dependent) variables.
@@ -142,7 +142,7 @@ PK_BDF_Default::AdvanceStep(double t_old, double t_new, bool reinit)
   // --  dt is the requested timestep size.  It must be less than or equal to...
   // --  dt_internal is the max valid dt, and is set by physics/solvers
   // --  dt_solver is what the solver wants to do
-  double dt_internal = S_->Get<double>(Keys::cleanName(name_) + "_dt_internal", Tags::DEFAULT);
+  double dt_internal = S_->Get<double>(Keys::cleanName(name_, true) + "_dt_internal", Tags::DEFAULT);
 
   // NOTE, still a bug in amanzi#685, despite fixes in amanzi#694, so this assertion still fails --ETC
   // AMANZI_ASSERT(dt <= dt_internal + 2.e-8); // roundoff
@@ -178,7 +178,7 @@ PK_BDF_Default::AdvanceStep(double t_old, double t_new, bool reinit)
       dt_internal = dt_solver;
     }
 
-    S_->Assign(Keys::cleanName(name_) + "_dt_internal", Tags::DEFAULT, name_, dt_internal);
+    S_->Assign(Keys::cleanName(name_, true) + "_dt_internal", Tags::DEFAULT, name_, dt_internal);
   } catch (Errors::TimeStepCrash& e) {
     // inject more information into the crash message
     std::stringstream msg_str;
@@ -199,7 +199,7 @@ PK_BDF_Default::AdvanceStep(double t_old, double t_new, bool reinit)
 void
 PK_BDF_Default::UpdateContinuationParameter(double lambda)
 {
-  S_->Assign(Keys::cleanName(name_) + "_continuation_parameter", Tags::DEFAULT, name_, lambda);
+  S_->Assign(Keys::cleanName(name_, true) + "_continuation_parameter", Tags::DEFAULT, name_, lambda);
   ChangedSolution();
 }
 
