178b9cd4a5c6c3d9d1500846df993f8f926d16ae
Phong Le <levuvietphong@users.noreply.github.com>
2025-07-08 12:38:48 -0400

    Fix C-Q overland and field evaluator bug: use cell concentration for outgoing flux (#314)
    Ensure that flux leaving the domain uses the current cell concentration, not the concentration from the C-Q relationship. This corrects the behavior at domain boundaries.
    
    Closes #276.

--- src/pks/transport/constitutive_relations/sources/qc_relation_field_evaluator.cc
+++ src/pks/transport/constitutive_relations/sources/qc_relation_field_evaluator.cc
@@ -26,6 +26,7 @@ QCRelationFieldEvaluator::QCRelationFieldEvaluator(Teuchos::ParameterList& plist
   cv_key_ = Keys::readKey(plist, domain_, "cell volume", "cell_volume");
   dependencies_.insert(KeyTag{ cv_key_, tag });
   molar_density_key_ = Keys::readKey(plist, domain_, "molar density liquid", "molar_density_liquid");
+  tcc_key_ = Keys::readKey(plist, domain_, "concentration", "total_component_concentration");
   dependencies_.insert(KeyTag{ molar_density_key_, tag });
   field_src_key_ = Keys::readKey(plist, domain_, "field source", "water_source_field");
   dependencies_.insert(KeyTag{ field_src_key_, tag });
@@ -49,30 +50,39 @@ QCRelationFieldEvaluator::Evaluate_(const State& S, const std::vector<CompositeV
   const auto& cv = *S.Get<CompositeVector>(cv_key_, tag).ViewComponent("cell", false);
   const auto& molar_den =
     *S.Get<CompositeVector>(molar_density_key_, tag).ViewComponent("cell", false);
+  const auto& tcc = *S.Get<CompositeVector>(tcc_key_, tag).ViewComponent("cell", false);    
   const auto& water_from_field =
     *S.Get<CompositeVector>(field_src_key_, tag).ViewComponent("cell", false);
   auto& surf_src = *result[0]->ViewComponent("cell"); // not being reference
   const AmanziMesh::Mesh& mesh = *result[0]->Mesh();
-  double field_flow, source_mass;
+  double field_flow, source_mass, tcc_current;
   
   // Loop through each cell
   AmanziMesh::Entity_ID ncells = cv.MyLength();
   for (AmanziMesh::Entity_ID c = 0; c != ncells; ++c) {
     
     if (extensive_) {
-      // convert extensive quantity in mol/m2/s to m3/s
+      // convert extensive quantity in mol/s to m3/s
       field_flow = water_from_field[0][c] / molar_den[0][c];
     } else {
-      // convert intensive quantity from mol/s to m3/s
+      // convert intensive quantity from mol/m2/s to m3/s
       field_flow = water_from_field[0][c] * cv[0][c] / molar_den[0][c];
     }
+    // current concentration
+    tcc_current = tcc[0][c];
 
-    // transport source (concentration g/m3) as a function of discharge from a field (e.g. tile, groundwater)
-    source_mass = (*QC_curve_)(std::vector<double>{field_flow});
-
-    // return solute mass rate by multiplying with discharge (molC/s)
-    // Here we assume the molar mass is 1. TODO: add molar mass to the function.
-    surf_src[0][c] = source_mass * field_flow * molar_den[0][c] / cv[0][c];
+    // transport source (concentration g/m3) as a function of discharge from a field
+    source_mass = (*QC_curve_)(std::vector<double>{std::abs(field_flow)});
+    
+    // temporarily assume molar mass is 1.0
+    // TODO: read molar mass from the xml file
+    if (field_flow > 0) {
+      // positive flux means source, concentration is from source_transport
+      surf_src[0][c] = source_mass * field_flow * 1.0;
+    } else {
+      // negative flux means sink, concentration is the same as the current concentration
+      surf_src[0][c] = tcc_current * field_flow * 1.0;
+    }    
   }
 }
 
