b02a13c9dc65b3b46b097f361dadbe3bd774efca
Daniil Svyatsky <dasvyat@lanl.gov>
2025-07-24 23:30:52 -0600

    Documentation for sediment transport and morphology evaluators (#317)

--- src/pks/transport/constitutive_relations/sediment_transport/erosion_evaluator.hh
+++ src/pks/transport/constitutive_relations/sediment_transport/erosion_evaluator.hh
@@ -7,9 +7,35 @@
   Authors: Daniil Svyatsky (dasvyat@lanl.gov)
 */
 
-/*
-  The erosion evaluator gets the erosion rates.
+/*!
 
+The erosion evaluator gets the erosion rates.  We evaluate the erosion flux,
+:math:`Q_e`, by way of a relationship which can be applied when the bed
+properties are relatively uniform over the depth and the bed is consolidated
+[e.g., Mehta, 1984], namely,
+
+.. math::
+
+   Q_e = \left\{\begin{array}{ll}
+                   Q_{e_0}\left(\frac{\tau_0}{\tau_e} - 1\right) &\qquad\mbox{if} \qquad \tau_0>\tau_e \\
+                   0  &\qquad\mbox{if} \qquad \tau_0\le\tau_e
+                \end{array} \right.
+
+where :math:`Q_{e_0}` is an empirical coefficient,  :math:`\tau_e` is a critical shear stress
+
+`"evaluator type`" = `"erosion rate`"
+
+.. _evaluator-erosion-rate-spec:
+.. admonition:: evaluator-erosion-rate-spec
+
+   * `"critical shear stress`" ``[double]`` **0.4**
+   * `"empirical coefficient`" ``[double]`` **3.0e-4**
+   * `"drag coefficient`" ``[double]`` **0.02**
+   * `"specific weight of water`" ``[double]`` **9806.0**
+
+   DEPENDENCIES:
+
+   - `"velocity`" **DOMAIN-velocity**
 
 */
 
--- src/pks/transport/constitutive_relations/sediment_transport/trapping_evaluator.hh
+++ src/pks/transport/constitutive_relations/sediment_transport/trapping_evaluator.hh
@@ -8,8 +8,42 @@
 */
 
 /*
-  The erosion evaluator gets the erosion rates.
+  The trapping evaluator gets the trapping rates.
+  we have expressed the trapping rate using the
+  approach proposed by Palmer et al. [2004],
 
+  ..math::
+  Q_{tr} = \rho_s\,C\,U\,\epsilon d_s n_s min(h_s, p_D)
+
+  ..math::
+  \epsilon = \alpha_e\left(\frac{Ud_s}{\nu}\right)^{\beta_\epsilon}
+             \left(\frac{d}{d_s}\right)^{\gamma_\epsilon}
+
+  where math::'\rho_s' is the sediment density, math::'C' in a sediment molar ratio,
+  math::'U' is the absolute value of the local flow speed, math::'\epsilon' is
+  a capture efficiency which gives the rate at which
+  transported sediment particles are captured by plant stems,
+  math::'d' is a particle diameter, math::'\nu' is the kinematic viscosity,
+  math::'d_s' is the stem diameter, math::'n_s' is the stem density,
+  math::'h_s' is the average height of the stems, and math::'p_D" is the local flow
+  ponded-depth
+
+  `"evaluator type`" = `"trapping rate`"
+
+   * `"kinematic viscosity`" ``[double]`` **1e-6**
+   * `"particle diameter`" ``[double]`` **5.e-5**
+   * `"alpha`" ``[double]`` **0.224**
+   * `"beta`" ``[double]`` **0.718**
+   * `"gamma`" ``[double]`` **2.08**
+
+     DEPENDENCIES:
+
+   - `"velocity`" **SURFACE-DOMAIN-velocity**
+   - `"sediment`" **SURFACE-DOMAIN-sediment**
+   - `"stem_diameter`" **SURFACE-DOMAIN-stem_diameter**
+   - `"stem_height`" **SURFACE-DOMAIN-stem_height**
+   - `"stem_density`" **SURFACE-DOMAIN-stem_density**
+   - `"ponded depth`" **SURFACE-DOMAIN-ponded_depth**
 
 */
 #pragma once
