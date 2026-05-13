3ee92a000f778601a98f0f05e53e0fc4a0f7415b
AndrewGraus <andrewsgraus@gmail.com>
2026-05-13 18:47:14 -0400

    reworking canopy snow dataset
    -reverted to using the column size scheme to add the canopy snow data.
    This is a bit hacky as we should be looping over num_pfts, but as long
    as the number of cells in a column > 5 this won't cause any issues.
    
    -Bug fix for how the memory for canopy snow was allocated, the wrong
    size was used, and it was grabbing random data to pass back to EcoSIM.

--- src/pks/ecosim/data/BGC_memory.cc
+++ src/pks/ecosim/data/BGC_memory.cc
@@ -342,8 +342,8 @@ void AllocateBGCState(const BGCSizes* const sizes,
    AllocateBGCVectorDouble(sizes->num_columns, &(state->evaporation_litter));
    AllocateBGCVectorDouble(sizes->num_columns, &(state->evaporation_snow));
    AllocateBGCVectorDouble(sizes->num_columns, &(state->sublimation_snow));
-   AllocateBGCMatrixDouble(sizes->num_columns, sizes->num_columns, &(state->snow_temperature));
-   AllocateBGCMatrixDouble(sizes->num_columns, sizes->num_pfts, &(state->canopy_snow));
+   AllocateBGCMatrixDouble(sizes->ncells_per_col_, sizes->num_columns, &(state->snow_temperature));
+   AllocateBGCMatrixDouble(sizes->ncells_per_col_, sizes->num_columns, &(state->canopy_snow));
    AllocateBGCTensorDouble(sizes->ncells_per_col_, sizes->num_columns, sizes->num_components, &(state->mole_fraction));
    //ALQUIMIA_ASSERT(state->total_mobile.data != NULL);
  }  /* end AllocateBGCState() */
